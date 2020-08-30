cf8f7eaf8c166e202716f8e20d474d9646102af6
Marco Cantu Gea <marco.cantu.g@gmail.com>
2020-08-30 09:50:25 -0500

    first commit

---
+++ CURActionsLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CURActionsLib
+{
+    public class Class1
+    {
+    }
+}
---
+++ consoleLibTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace consoleLibTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("this is a added test");
+        }
+    }
+}
